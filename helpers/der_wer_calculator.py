17e60150e4e58dd796ee6f3dc394745ce6da9eeb
Ibraheem Tuffaha <bro.t.1996@gmail.com>
2019-07-09 07:41:53 +0300

    Add Sentence Error Rate to the calculator

--- helpers/der_wer_calculator.py
+++ helpers/der_wer_calculator.py
@@ -143,6 +143,88 @@ def calculate_wer(original_file, target_file, arabic_letters, diacritic_classes,
 
   return round(not_equal / max(1, (equal + not_equal)) * 100, 2)
 
+def calculate_der(original_file, target_file, arabic_letters, diacritic_classes, style, case_ending=True, no_diacritic=True):
+  with open(original_file, 'r') as file:
+    original_content = file.readlines()
+
+  with open(target_file, 'r') as file:
+    target_content = file.readlines()
+
+  assert(len(original_content) == len(target_content))
+
+  equal = 0
+  not_equal = 0
+  for (original_line, target_line) in zip(original_content, target_content):
+    if style == 'Fadel':
+      original_line = clear_line(original_line, arabic_letters, diacritic_classes)
+      target_line = clear_line(target_line, arabic_letters, diacritic_classes)
+
+    original_classes = get_diacritics_classes(original_line, case_ending, arabic_letters, diacritic_classes, style)
+    target_classes = get_diacritics_classes(target_line, case_ending, arabic_letters, diacritic_classes, style)
+
+    assert(len(original_classes) == len(target_classes))
+
+    for (original_class, target_class) in zip(original_classes, target_classes):
+      if not no_diacritic and original_class == 0:
+        continue
+      if original_class == -1 and target_class != -1:
+        print('WOW!')
+      if original_class != -1 and target_class == -1:
+        print('WOW!')
+      if original_class == -1 and target_class == -1:
+        continue
+
+      equal += (original_class == target_class)
+      not_equal += (original_class != target_class)
+
+  return round(not_equal / max(1, (equal + not_equal)) * 100, 2)
+
+def calculate_ser(original_file, target_file, arabic_letters, diacritic_classes, style, case_ending=True, no_diacritic=True):
+  with open(original_file, 'r') as file:
+    original_content = file.readlines()
+
+  with open(target_file, 'r') as file:
+    target_content = file.readlines()
+
+  assert(len(original_content) == len(target_content))
+
+  equal = 0
+  not_equal = 0
+  for idx, (original_line, target_line) in enumerate(zip(original_content, target_content)):
+    if style == 'Fadel':
+      original_line = clear_line(original_line, arabic_letters, diacritic_classes)
+      target_line = clear_line(target_line, arabic_letters, diacritic_classes)
+
+    original_line = original_line.split()
+    target_line = target_line.split()
+
+    assert(len(original_line) == len(target_line))
+
+    equal_words = True
+    for (original_word, target_word) in zip(original_line, target_line):
+      original_classes = get_diacritics_classes(original_word, case_ending, arabic_letters, diacritic_classes, style)
+      target_classes = get_diacritics_classes(target_word, case_ending, arabic_letters, diacritic_classes, style)
+
+      assert(len(original_classes) == len(target_classes))
+
+      if len(original_classes) == 0:
+        continue
+
+      equal_classes = 0
+      for (original_class, target_class) in zip(original_classes, target_classes):
+        if not no_diacritic and original_class == 0:
+          equal_classes += 1
+          continue
+        equal_classes += (original_class == target_class)
+
+      if equal_classes != len(original_classes):
+        equal_words = False
+
+    equal += equal_words
+    not_equal += not equal_words
+
+  return round(not_equal / max(1, (equal + not_equal)) * 100, 2)
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Calculate DER and WER')
   parser.add_argument('-ofp', '--original-file-path', help='File path to original text', required=True)
@@ -179,3 +261,16 @@ if __name__ == '__main__':
         calculate_wer(args.original_file_path, args.target_file_path, ARABIC_LETTERS_LIST, CLASSES_LIST, args.style, no_diacritic=False),
         calculate_wer(args.original_file_path, args.target_file_path, ARABIC_LETTERS_LIST, CLASSES_LIST, args.style, case_ending=False, no_diacritic=False)))
   print('+---------------------------------------------------------------------------------------------+')
+  print('')
+  print('+---------------------------------------------------------------------------------------------+')
+  print('|       |  With case ending  | Without case ending |  With case ending  | Without case ending |')
+  print('|  SER  |------------------------------------------+------------------------------------------|')
+  print('|       |          Including no diacritic          |          Excluding no diacritic          |')
+  print('|-------+------------------------------------------+------------------------------------------|')
+  print('|   %%   |        %5.2f       |        %5.2f        |        %5.2f       |        %5.2f        |' %
+        (calculate_ser(args.original_file_path, args.target_file_path, ARABIC_LETTERS_LIST, CLASSES_LIST, args.style),
+        calculate_ser(args.original_file_path, args.target_file_path, ARABIC_LETTERS_LIST, CLASSES_LIST, args.style, case_ending=False),
+        calculate_ser(args.original_file_path, args.target_file_path, ARABIC_LETTERS_LIST, CLASSES_LIST, args.style, no_diacritic=False),
+        calculate_ser(args.original_file_path, args.target_file_path, ARABIC_LETTERS_LIST, CLASSES_LIST, args.style, case_ending=False, no_diacritic=False)))
+  print('+---------------------------------------------------------------------------------------------+')
+  
